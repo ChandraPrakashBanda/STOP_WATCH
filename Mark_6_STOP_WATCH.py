83a6ee1098ae9c628abd8e67f7346c34a7723f2e
saidudeprakash <67602636+saidudeprakash@users.noreply.github.com>
2020-08-08 21:31:58 +0530

    Update Mark_6_STOP_WATCH.py

--- Mark_6_STOP_WATCH.py
+++ Mark_6_STOP_WATCH.py
@@ -10,24 +10,22 @@ def countdown(count):
     # counting time 
     seconds=math.floor(count%60)
     minutes=math.floor((count/60)%60)
-    hours=math.floor((count/3600))
-    label=Label(root,text="Hours: "+str(hours)+"Minutes: "+str(minutes)+"Seconds: "+str(seconds))
+    label=Label(root,text="Minutes: "+str(minutes)+"Seconds: "+str(seconds))
     label.pack()
 
     if count>=0:
         root.after(1000,countdown,count-1)
     else:
         for x in range(3):
-            winsound.Beep(1000,1000)
+            winsound.Beep(1000,3000)
         label1=Label(root,text="Time is up")
         label1.pack()
 def updateButton():
         # taking entries 
-        hour=set_hour.get()
         minute=set_minute.get()
         second=set_second.get()
         #Calling countdown function
-        if(hour.isdigit()and minute.isdigit and second.isdigit):
+        if(minute.isdigit and second.isdigit):
             time=int(hour)*3600+int(minute)*60+int(second)
             countdown(time)
 
@@ -35,8 +33,6 @@ root=Tk()
 root.geometry("400x400+50+50")
 root.title("STOP_WATCH")
 #creating entries
-set_hour=Entry(root)
-set_hour.pack()
 set_minute=Entry(root)
 set_minute.pack()
 set_second=Entry(root)
